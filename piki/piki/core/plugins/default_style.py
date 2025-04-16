abdb90966a815f7ffccc83c4661a9d648647e88c
Gonçalo MB <me@goncalomb.com>
2025-04-16 19:08:35 +0100

    Change default style header title

--- piki/piki/core/plugins/default_style.py
+++ piki/piki/core/plugins/default_style.py
@@ -1,11 +1,11 @@
 import urwid
-from piki.plugin import Plugin
-
 from piki import piki_source_url, piki_version
+from piki.plugin import Plugin
 
 
 class DefaultStylePlugin(Plugin):
-    _header_message = "PiKi: Raspberry [Pi Ki]osk"
+    _title = "PiKi"
+    _header_message = "A Raspberry [Pi Ki]osk"
     _footer_message = "piki v%s \N{BULLET} %s" % (
         piki_version, piki_source_url,
     )
@@ -28,6 +28,9 @@ class DefaultStylePlugin(Plugin):
         )
         # create header and footer pile
         ui.w_frame.header = urwid.Filler(urwid.Pile([
+            urwid.Padding(urwid.BigText(
+                self._title, urwid.HalfBlock5x4Font(),
+            ), 'center', 'clip'),
             urwid.Text(self._header_message, 'center'),
         ]), top=1, bottom=1)
         ui.w_frame.footer = urwid.Filler(urwid.Pile([
